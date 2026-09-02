e2689c1579bc386ab2c2096b859a6fb076830984
Jake Banks <jakewbanks@gmail.com>
2026-09-02 07:50:58 -0700

    fix: restore sidebar link styling after routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
+import './routing.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
 if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sws-fleet/sw.js')
